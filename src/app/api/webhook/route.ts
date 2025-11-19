f5c32d5d1ce59aeb386d92e165b71545e0a9587f
Ipsa1504 <ipsabadoniya@gmail.com>
2025-11-19 12:04:38 +0530

    Make changes in user model

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,10 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
 
 // 🚨 REQUIRED for Stripe Webhooks to work
-export const runtime = "nodejs";
-export const preferredRegion = "auto";
-
-
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 export async function POST(req: NextRequest) {
   if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_SIGNING_SECRET) {
     return NextResponse.json(
--- src/app/models/Category.ts
+++ src/app/models/Category.ts
@@ -1,8 +1,20 @@
 import { Schema, model, models } from "mongoose";
 
-const CategorySchema = new Schema({
-  name: { type: String, require: true, unique: true },
-  image: { type: String },
-}, { timestamps: true });
+const CategorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-export const Category = models?.Category || model('Category', CategorySchema);
+// FIX: removes TypeScript “complex union” error
+export const Category = (models.Category as any) || model("Category", CategorySchema);
+
+export default Category;
